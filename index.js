6cbd0f51efa27162aa4da22003f67f6fcff8b66a
Dorsa Sadat Zarrabi <72649045+dorsa-zarrabi@users.noreply.github.com>
2020-12-31 21:48:41 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./style.css";
+import App from './App.js';
+import footer from './footer.js'
+import list from './list.js';
+import update from './update.js'
+import tala from './tala.js';
+import "bootstrap/assts/booystrap.min.css";
 import './style.css';
-import app from './app';
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
