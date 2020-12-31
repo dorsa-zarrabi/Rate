5d31966a744345b2ed1ce861bd5adbb25f950db8
Dorsa Sadat Zarrabi <72649045+dorsa-zarrabi@users.noreply.github.com>
2020-12-31 21:21:41 +0330

    Add files via upload

---
+++ assts/convert.js
@@ -0,0 +1,48 @@
+const CodeToparcham = {
+    alman: "alman",
+    amrica: "amrica",
+	araq: "araq",
+    ARGENTIN: "ARGENTIN,
+    berezil: "berezil",
+    canada: "canada",
+    faranse: "faranse",
+	iran: "iran",
+    norvezh: "norvezh",
+    ostoralia: "ostoralia",
+    otrish: "otrish",
+    zhapon: "zhapon",
+    'mesghal': "ounce",
+    'tala18': "ounce",
+    'bitcoin;: "bitcoin",
+	'1old azadi': "1oldazadi",
+    '1emami': "1oldazadi",
+    '1-2 azadi': "1oldazadi",
+    '1-4 azadi': "1oldazadi";
+}
+
+
+const CodeToCountry = {
+    amrica: "دلار آمریکا",
+    youro: "یورو",
+    pound : "پوند",
+    koron : "کرون",
+	rial : "ریال"
+    yen: "ین";
+}
+
+
+const FarsiNametala = {
+    '1old azadi' : "سکه بهار آزادی",
+    '1emami' : "سکه امامی",
+    '1-2 azadi' : "نیم سکه آزادی",
+    '1-4 azadi' : "ربع سکه بهار آزادی",
+    'mesghal' : "مثقال",
+    "tala18" : "طلا 18 عیار",
+    "bitcoin" : "بیت کوین",
+}
+
+export {
+    CodeToCountry,
+    CodeToparcham,
+    FarsiNametala
+};
